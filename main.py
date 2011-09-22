c28b26dbd82040a5f8ab4beeecf828628a202b45
Igor Karpov <ikarpov@cs.utexas.edu>
2011-09-22 18:00:46 -0500

    merge -r 1105:1109 from opennero trunk

--- main.py
+++ main.py
@@ -3,8 +3,3 @@ from BlocksPlanning.client import ClientMain
 
 def ModMain():
     ClientMain()
-
-def StartMe():
-    from BlocksPlanning.module import getMod
-    getMod().set_speedup(1.0) # full speed ahead
-    getMod().start_sarsa() # start an algorithm for headless mode
